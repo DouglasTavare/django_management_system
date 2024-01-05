050f9a9b455e2fc96cc16a9c03423d039cfc57de
Douglas Tavares <tavares-douglas@outlook.com>
2024-01-05 19:05:50 -0300

    fix: fixes API methods tests adding authentication in its requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest  
   
-from rest_framework.test import APIClient  
-  
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
   
 @pytest.fixture(scope="function")  
 def api_client() -> APIClient:  
@@ -10,3 +11,14 @@ def api_client() -> APIClient:
     :return: APIClient  
     """  
     yield APIClient()
+
+@pytest.fixture
+def authenticated_api_client():
+    user = User.objects.create_user(username='testuser', password='testpassword')
+    refresh = RefreshToken.for_user(user)
+    access_token = str(refresh.access_token)
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
+    return client
--- tests/unit/test_product.py
+++ tests/unit/test_product.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 import pytest
+
 """
 The following annotation ensures the database is set up correctly for testing.
 Each test will run in its own transaction, which will be rolled back at the end of the test.
 """
+
 @pytest.mark.django_db
-def test_create_product(api_client) -> None:  
+def test_create_product(authenticated_api_client) -> None:  
     """  
     Test the create product API  
     :param api_client:  
@@ -16,20 +19,20 @@ def test_create_product(api_client) -> None:
         "description": "Test product description",
         "price": 650
     }
-  
+
     # Create a product  
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     product_id = response_create.data["id"]  
     assert response_create.status_code == 201  
     assert response_create.data["name"] == payload["name"]  
   
     # Read the product data
-    response_read = api_client.get(f"/products/{product_id}", format="json")  
+    response_read = authenticated_api_client.get(f"/products/{product_id}", format="json")  
     assert response_read.status_code == 200  
     assert response_read.data["name"] == payload["name"]
 
 @pytest.mark.django_db  
-def test_create_product_with_invalid_data(api_client) -> None:
+def test_create_product_with_invalid_data(authenticated_api_client) -> None:
     """  
     Test the create product API  
     :param api_client:  
@@ -44,7 +47,7 @@ def test_create_product_with_invalid_data(api_client) -> None:
     # Create a product with invalid value for name field (name must have at least 3 characters)
     payload = default_payload
     payload['name'] = 'ab'
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     assert response_create.status_code == 400 
     assert response_create.data["name"][0].title() == "Field 'Name' Must Have At Least 3 Characters"
     
@@ -52,19 +55,19 @@ def test_create_product_with_invalid_data(api_client) -> None:
     # Create a product with invalid value for description field (name must have at least 10 characters)
     payload = default_payload
     payload['description'] = 'descrip'
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     assert response_create.status_code == 400 
     assert response_create.data["description"][0].title() == "Field 'Description' Must Have At Least 10 Characters"
   
     # Create a product with invalid value for price field (price must be at least 500)
     payload = default_payload
     payload['price'] = 499
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     assert response_create.status_code == 400 
     assert response_create.data["price"][0].title() == "Field 'Price' Must Be Higher Than 500"
 
 @pytest.mark.django_db  
-def test_get_product(api_client) -> None:
+def test_get_product(authenticated_api_client) -> None:
     """  
     Test the update product API  
     :param api_client:  
@@ -77,7 +80,7 @@ def test_get_product(api_client) -> None:
     }
   
     # Create a product  
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     assert response_create.status_code == 201  
     assert response_create.data["name"] == payload["name"] 
 
@@ -88,22 +91,22 @@ def test_get_product(api_client) -> None:
     }
   
     # Create a product  
-    response_create = api_client.post("/products/", data=payload, format="json")
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")
     product_id = response_create.data["id"]  
     assert response_create.status_code == 201  
     assert response_create.data["name"] == payload["name"]
 
     # Read the product data
-    response_read = api_client.get(f"/products/{product_id}", format="json")  
+    response_read = authenticated_api_client.get(f"/products/{product_id}", format="json")  
     assert response_read.status_code == 200  
     assert response_read.data["name"] == payload["name"]
 
-    response_read = api_client.get(f"/products/", format="json")  
+    response_read = authenticated_api_client.get(f"/products/", format="json")  
     assert response_read.status_code == 200  
     assert len(response_read.data["results"]) == 2
 
 @pytest.mark.django_db  
-def test_patch_product(api_client) -> None:  
+def test_patch_product(authenticated_api_client) -> None:  
     """  
     Test the update product API  
     :param api_client:  
@@ -116,26 +119,26 @@ def test_patch_product(api_client) -> None:
     }
   
     # Create a product  
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     product_id = response_create.data["id"]  
     assert response_create.status_code == 201  
     assert response_create.data["name"] == payload["name"] 
 
     # Update the product  
-    response_update = api_client.patch(  
+    response_update = authenticated_api_client.patch(  
         f"/products/{product_id}", data={"name": "Test product"}, format="json"  
     )  
     assert response_update.status_code == 200
     assert response_update.data["name"] == payload["name"]
   
     # Product doesn't exist  
-    response_update = api_client.patch(  
+    response_update = authenticated_api_client.patch(  
         f"/products/{product_id + 1}", data=payload, format="json"  
     )  
     assert response_update.status_code == 404
 
 @pytest.mark.django_db  
-def test_put_product(api_client) -> None:  
+def test_put_product(authenticated_api_client) -> None:  
     """  
     Test the update product API  
     :param api_client:  
@@ -148,27 +151,27 @@ def test_put_product(api_client) -> None:
     }
   
     # Create a product  
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     product_id = response_create.data["id"]  
     assert response_create.status_code == 201  
     assert response_create.data["name"] == payload["name"] 
   
     # Update the product  
     payload["name"] = "Updated test product"  
-    response_update = api_client.put(  
+    response_update = authenticated_api_client.put(  
         f"/products/{product_id}", data=payload, format="json"  
     )  
     assert response_update.status_code == 200
     assert response_update.data["name"] == payload["name"]
   
     # Product doesn't exist  
-    response_update = api_client.put(  
+    response_update = authenticated_api_client.put(  
         f"/products/{product_id + 1}", data=payload, format="json"  
     )  
     assert response_update.status_code == 404
 
 @pytest.mark.django_db  
-def test_delete_product(api_client) -> None:  
+def test_delete_product(authenticated_api_client) -> None:  
     """  
     Test the delete product API  
     :param api_client:  
@@ -181,19 +184,19 @@ def test_delete_product(api_client) -> None:
     }  
   
     # Create a product  
-    response_create = api_client.post("/products/", data=payload, format="json")  
+    response_create = authenticated_api_client.post("/products/", data=payload, format="json")  
     product_id = response_create.data["id"]  
     assert response_create.status_code == 201  
     assert response_create.data["name"] == payload["name"] 
   
     # Delete the product  
-    response_delete = api_client.delete(f"/products/{product_id}", format="json")  
+    response_delete = authenticated_api_client.delete(f"/products/{product_id}", format="json")  
     assert response_delete.status_code == 204  
   
     # Read the product  
-    response_read = api_client.get(f"/products/{product_id}", format="json")  
+    response_read = authenticated_api_client.get(f"/products/{product_id}", format="json")  
     assert response_read.status_code == 404  
   
     # product doesn't exist  
-    response_delete = api_client.delete(f"/products/{product_id + 1}", format="json")  
+    response_delete = authenticated_api_client.delete(f"/products/{product_id + 1}", format="json")  
     assert response_delete.status_code == 404

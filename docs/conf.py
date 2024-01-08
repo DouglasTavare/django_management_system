ae867c99ee4fdb90db00519602772d2ae55ce78b
Douglas Tavares <tavares-douglas@outlook.com>
2024-01-08 12:10:19 -0300

    fix: adds black formatting to docs config file

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,9 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'django_management_system.settings'
+
+sys.path.insert(0, os.path.abspath(".."))
+os.environ["DJANGO_SETTINGS_MODULE"] = "django_management_system.settings"
 django.setup()
 
 project = "Django Management System"
